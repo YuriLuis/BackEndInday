2ca65c50d5a485c49c73bede9e938dbc910d4df5
YuriLuis <yuri.luizg@hotmail.com>
2019-11-13 19:41:25 -0300

    Projeto criado

---
+++ src/main/java/com/Inday/indaybackend/IndaybackendApplication.java
@@ -0,0 +1,13 @@
+package com.Inday.indaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndaybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndaybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Inday/indaybackend/IndaybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Inday.indaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndaybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
