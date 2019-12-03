ffbf10b558ebb3205622ddf43c50c5a2170cf3c5
JackMaul <jacksonmmaul@gmail.com>
2019-12-03 20:37:15 -0300

    Corrgindo @CrossOrigin(*)

--- src/main/java/com/Inday/indaybackend/controller/CategoriaController.java
+++ src/main/java/com/Inday/indaybackend/controller/CategoriaController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.Inday.indaybackend.repository.CategoriaRepository;
 
 @RestController
 @RequestMapping("/categoria")
+@CrossOrigin("*")
 public class CategoriaController {
 	
 	
@@ -35,7 +37,7 @@ public class CategoriaController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<?> salvar(@RequestBody @Valid Categoria categoria) {	
+	public ResponseEntity<?> salvar(@RequestBody  Categoria categoria) {	
 		
 		if (repository.findByDescricao(categoria.getDescricao()) == null) {
 			
